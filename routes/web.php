6010b26e8781fa6f594138a3e872d0d5241f51a7
calsob <admin@calsob.com>
2023-08-15 14:18:51 +0530

    thankyou page report code removed

--- routes/web.php
+++ routes/web.php
@@ -108,37 +108,38 @@ Route::group(['middleware'=> 'coming_soon'], function(){
   Route::resource('start_quiz/{id}/quiz', 'MainQuizController');
 
   Route::get('start_quiz/{id}/finish', function($id){
-    $auth = Auth::user();
-    $topic = Topic::findOrFail($id);
-    $questions = Question::where('topic_id', $id)->get();
-    $count_questions = $questions->count();
-    $answers = Answer::where('user_id',$auth->id)
-                ->where('topic_id',$id)->get(); 
+    // $auth = Auth::user();
+    // $topic = Topic::findOrFail($id);
+    // $questions = Question::where('topic_id', $id)->get();
+    // $count_questions = $questions->count();
+    // $answers = Answer::where('user_id',$auth->id)
+    //             ->where('topic_id',$id)->get(); 
 
-    if($count_questions != $answers->count()){
-      foreach($questions as $que){ 
-        $a = false;   
-        foreach($answers as $ans){ 
-          if($que->id == $ans->question_id){ 
-            $a = true;
-          }
-        }
-        if($a == false){  
-          Answer::create([
-            'topic_id' => $id,
-            'user_id' => $auth->id,
-            'question_id' => $que->id,
-            'user_answer' => 0,
-            'answer' => $que->answer,
-          ]);
-        }
-      }
-    }
+    // if($count_questions != $answers->count()){
+    //   foreach($questions as $que){ 
+    //     $a = false;   
+    //     foreach($answers as $ans){ 
+    //       if($que->id == $ans->question_id){ 
+    //         $a = true;
+    //       }
+    //     }
+    //     if($a == false){  
+    //       Answer::create([
+    //         'topic_id' => $id,
+    //         'user_id' => $auth->id,
+    //         'question_id' => $que->id,
+    //         'user_answer' => 0,
+    //         'answer' => $que->answer,
+    //       ]);
+    //     }
+    //   }
+    // }
 
-    $ans= Answer::all();
-    $q= Question::all();
+    // $ans= Answer::all();
+    // $q= Question::all();
     
-    return view('finish', compact('ans','q','topic', 'answers', 'count_questions'));
+    // return view('finish', compact('ans','q','topic', 'answers', 'count_questions'));
+    return view('finish');
   });
 
   Route::get('admin/moresettings/socialicons/','SocialController@index')->name('socialicons.index');
